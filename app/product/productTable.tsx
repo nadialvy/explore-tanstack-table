c19c4acdccbf269644bc757ed5107ba64716e5c1
nadialvy <5026231007@student.its.ac.id>
2025-10-21 00:18:40 +0700

    feat: add button to share image from public URL in ProductTable

--- app/product/productTable.tsx
+++ app/product/productTable.tsx
@@ -55,6 +55,17 @@ export default function ProductTable() {
         Share Image
       </button>
       <button
+        className=" bg-blue-400 px-4 py-2 rounded text-white mr-2"
+        onClick={() =>
+          shareMediaFile({
+            url: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg",
+          })
+        }
+      >
+        Share Image from public url
+      </button>
+
+      <button
         className=" bg-green-400 px-4 py-2 rounded text-white"
         onClick={() => shareMediaFile({ url: "/cat.mp4", type: "video" })}
       >
@@ -71,7 +82,6 @@ export default function ProductTable() {
       >
         Share Video Public URl
       </button>
-
       {/* <BasicTable data={data} columns={columns} /> */}
     </div>
   );
